ab8d9b0878b06824227c94142625a7f8375c0f81
Harry Pham <htpham3096@gmail.com>
2023-03-11 17:26:50 +0700

    social management post management

---
+++ core/routers.py
@@ -0,0 +1,18 @@
+from rest_framework import routers
+from core.user.viewsets import UserViewSet
+from core.auth.viewsets.register import RegisterViewSet
+from core.auth.viewsets.login import LoginViewSet
+from core.auth.viewsets.refresh import RefreshViewSet
+from core.post.viewsets import PostViewSet
+
+router = routers.SimpleRouter()
+# Auth
+router.register(r"auth/register", RegisterViewSet, basename="auth-register")
+router.register(r"auth/login", LoginViewSet, basename="auth-login")
+router.register(r"auth/refresh", RefreshViewSet, basename="auth-refresh")
+# user
+router.register(r"user", UserViewSet, basename="user")
+# post
+router.register(r"post", PostViewSet, basename="post")
+
+urlpatterns = [*router.urls]
